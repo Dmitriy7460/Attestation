45ca4715e7347a2b8a2fe49caa45be30afa31434
Дмитрий <matveev_dmitriy1996@mail.ru>
2023-05-23 00:16:14 +0300

    Completed Program V1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше или равна 3 символа
+
+int lengthOfString = 4; 
+char[] exceptions = new char[]{',', '.', ' ', ';'};
+
+Console.WriteLine("Введи значения через пробел, точку, запятую или точку с запятой: ");
+
+string[] specifiedArray = Input(exceptions);
+string[] filtrationArray = Filtration(specifiedArray, Sum (specifiedArray,lengthOfString), lengthOfString);
+Output(filtrationArray);
+
+
+
+
+
+//Console.WriteLine($"Сумма = {Sum (specifiedArray,lengthOfString)}");
+
+
+
+//\\//\\//\\//\\//\\//\\//\\//\\
+
+string[] Input (char[]exp)
+{
+    string num = Console.ReadLine();
+    string[] stringOfNum = num.Split(exp);
+
+    return stringOfNum;
+}
+
+int Sum (string[]a, int los)
+{
+    int j = 0;
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+         if (a[i].Length < los && a[i] !="") j++;//////////////
+    }
+    return j;
+}
+
+string[] Filtration(string[]a, int size, int los)
+{
+    int j = 0;
+    string [] b = new string[size];
+
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        if (a[i].Length < los && a[i] !="") ///////////////////
+        {
+            b[j] = a[i]; 
+            j++;
+        }
+    }
+    return b;
+}
+
+void Output(string[]str)
+{
+for (int i = 0; i < str.GetLength(0); i++)
+{
+    Console.Write ($"{str[i]}, ");
+}
+Console.Write("\b\b.");
+}
